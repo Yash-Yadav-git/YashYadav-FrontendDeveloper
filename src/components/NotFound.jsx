a0b8c06c6dd0099376cf546650540f5d7afae897
Yash-Yadav-git <yashyadavgit@gmail.com>
2022-11-06 00:22:22 +0530

    Not Found page changed

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const NotFound = () => {
   return (
     <div class="flex items-center justify-center w-screen h-48 ">
-      <p class="text-5xl text-white md:text-7xl lg:text-9xl">Data Not Found</p>
+      <p class="text-3xl text-white md:text-7xl lg:text-9xl">
+        Search did not return any results..
+      </p>
+      <p class="text-2xl text-white md:text-7xl lg:text-9xl">Please retry.. </p>
     </div>
   );
 };
